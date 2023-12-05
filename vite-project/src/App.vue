97d4bef828e9b327c57e833de87cc624e1bd8d34
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-12-05 17:15:23 +0100

    step4

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -5,37 +5,38 @@ import axios from 'axios';
 import { store } from "./store.js";
 
 export default {
-  components: {
-    AppHeader,
-    AppMain
-  },
-  data() {
-    return {
-      store,
-    }
-  },
-  mounted() {
-    this.findLinks();
-  },
-  methods: {
-    findLinks() {
-      let options = {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/authentication',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDUzZGYyN2Y4ZTBmNThiMGQxOWI5M2E1ZmQ0Y2Q4YyIsInN1YiI6IjY1NmRlY2QwYTdlMzYzMDEzYWRlNGUzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P5X9yZQw5w-pgtY5q5LziNWW2Qnjzr8T2yLVoBk6Hlo'
-        }
-      };
+	components: {
+		AppHeader,
+		AppMain,
+		AppSearch
+	},
+	data() {
+		return {
+			store,
+		}
+	},
+	mounted() {
+		this.findLinks();
+	},
+	methods: {
+		findLinks() {
+			let options = {
+				method: 'GET',
+				url: 'https://api.themoviedb.org/3/authentication',
+				headers: {
+					accept: 'application/json',
+					Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDUzZGYyN2Y4ZTBmNThiMGQxOWI5M2E1ZmQ0Y2Q4YyIsInN1YiI6IjY1NmRlY2QwYTdlMzYzMDEzYWRlNGUzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P5X9yZQw5w-pgtY5q5LziNWW2Qnjzr8T2yLVoBk6Hlo'
+				}
+			};
 
-      axios.request(options).then(function (response) {
-        console.log(response.data);
-      })
-        .catch(function (error) {
-          console.error(error);
-        });
-    }
-  }
+			axios.request(options).then(function (response) {
+				console.log(response.data);
+			})
+				.catch(function (error) {
+					console.error(error);
+				});
+		}
+	}
 }
 
 
@@ -43,8 +44,8 @@ export default {
 </script>
 
 <template>
-  <AppHeader />
-  <AppMain />
+	<AppHeader />
+	<AppMain />
 </template>
 
 <style scoped></style>
---
+++ vite-project/src/components/Appsearch.vue
@@ -0,0 +1,25 @@
+export default {
+    name: "AppSearch",
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        search() {
+            console.log("Cerca: ", this.store.searchString);
+            this.$emit("search");
+        }
+    },
+}
+</script>
+
+<template>
+    <div>
+        <h1>the 10 best breweries in Austria</h1>
+        <input type="text" placeholder="search..." v-model="store.searchString" @keyup.enter="$emit('search')">
+        <button @click="search">search</button>
+    </div>
+</template>
+
+<style scoped></style>
