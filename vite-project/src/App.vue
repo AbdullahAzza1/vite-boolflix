173dd25c8253d6fc3bdee31443cd02348c52b655
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-12-05 17:05:18 +0100

    step3

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -13,6 +13,28 @@ export default {
     return {
       store,
     }
+  },
+  mounted() {
+    this.findLinks();
+  },
+  methods: {
+    findLinks() {
+      let options = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/authentication',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDUzZGYyN2Y4ZTBmNThiMGQxOWI5M2E1ZmQ0Y2Q4YyIsInN1YiI6IjY1NmRlY2QwYTdlMzYzMDEzYWRlNGUzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P5X9yZQw5w-pgtY5q5LziNWW2Qnjzr8T2yLVoBk6Hlo'
+        }
+      };
+
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+      })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
   }
 }
 
