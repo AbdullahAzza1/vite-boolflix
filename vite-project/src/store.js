d3bc6c8bc3814c1cb1f25a77d2236d5257a4be94
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-12-05 16:40:57 +0100

    step1

--- vite-project/src/store.js
+++ vite-project/src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    apiUrl: "",
-    films: [],
-
+    apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro",
+    movies: [],
+    searchString: ""
 });
